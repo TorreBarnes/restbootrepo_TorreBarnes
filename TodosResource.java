413426e867c83e23508578df0e71958d4e357ef3
Torre Barnes <torrebarnes@gmail.com>
2018-10-23 16:59:46 -0400

    Add Create ToDo annotation

--- TodosResource.java
+++ TodosResource.java
@@ -63,6 +63,8 @@ public class TodosResource {
 	//         Note that id will be auto-generated 
 	//            in the Todos database table
 	// Accept: application/json
+	
+	@RequestMapping(method = RequestMethod.POST, consumes = {"application/json"})
 	public Todo createTodo(@RequestBody Todo todo) {
 		Todo savedTodo = todoRepository.save(todo);
 		return savedTodo;
